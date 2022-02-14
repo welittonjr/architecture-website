27394c08a009018ecceef355ea695b979538d92d
Wellington Junior <wellington.junior@madeiramadeira.com.br>
2022-02-14 08:21:49 -0300

    funçao de proximo slides, ou anterior

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide-box");
+    if(n >= slides.length) {
+        slideIndex = 1;
+    } 
+    if (n < 1) {
+        slideIndex = slides.length;
+    } 
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    console.log(n-1);
+    slides[slideIndex-1].style.display = "flex";
+}
+
+
 function openMenu() {
     document.getElementById("navbar").style.display = "flex";
 }
