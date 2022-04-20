03eb08c6aaed91e2d8a02a172e0ce24a32cdf871
Wellington Junior <wellington.junior@madeiramadeira.com.br>
2022-04-19 23:36:53 -0300

    feature: form de instalaçao

--- app/Views/install_form.php
+++ app/Views/install_form.php
@@ -1,5 +1,4 @@
 <?= $this->extend('layouts/install/default') ?>
-namespace App\Views;
 
 <?= $this->section('content') ?>
 
@@ -10,18 +9,30 @@ namespace App\Views;
             <p class="h5 text-dark">Instalação</p>
         </div>
         <div class="card-body">
-            <?= form_open('/install') ?>
+            <?= form_open(base_url('install/it_started')) ?>
             <div class="form-group mb-3">
-                <?= form_input(array('class' => 'form-control', 'name' => 'hostname', 'placeholder' => 'Hostname', 'value' => set_value('hostname', 'localhost'))); ?>
+                <input type="text" class="form-control" name="hostname" placeholder="Hostname" value="<?= old('hostname') ?>">
+                <?php if (isset($error) && $error->hasError('hostname')): ?>
+                    <span class="text-danger"><?= $error->getError('hostname'); ?></span>
+                <?php endif; ?>
             </div>
             <div class="form-group mb-3">
-                <?= form_input(array('class' => 'form-control', 'name' => 'database', 'placeholder' => 'Database Name', 'value' => set_value('database'))); ?>
+                <input type="text" class="form-control" name="database" placeholder="Database Name" value="<?= old('database') ?>">
+                <?php if (isset($error) && $error->hasError('database')): ?>
+                    <span class="text-danger"><?= $error->getError('database'); ?></span>
+                <?php endif; ?>
             </div>
             <div class="form-group mb-3">
-                <?= form_input(array('class' => 'form-control', 'name' => 'username', 'placeholder' => 'Username', 'value' => set_value('username'))); ?>
+                <input type="text" class="form-control" name="username" placeholder="Username" value="<?= old('username') ?>">
+                <?php if (isset($error) && $error->hasError('username')): ?>
+                    <span class="text-danger"><?= $error->getError('username'); ?></span>
+                <?php endif; ?>
             </div>
             <div class="form-group mb-3">
-                <?= form_input(array('class' => 'form-control', 'name' => 'password', 'placeholder' => 'Password', 'value' => set_value('password'))); ?>
+                <input type="text" class="form-control" name="password" placeholder="Password" value="<?= old('password') ?>">
+                <?php if (isset($error) && $error->hasError('password')): ?>
+                    <span class="text-danger"><?= $error->getError('password'); ?></span>
+                <?php endif; ?>
             </div>
             <div class="row">
                 <div class="col-12">
@@ -30,7 +41,6 @@ namespace App\Views;
             </div>
             <?= form_close() ?>
         </div>
-
     </div>
 
 </div>
