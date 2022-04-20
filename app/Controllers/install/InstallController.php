9f4402cbdcac1cfaef5fe1d0b253e724eb771914
Wellington Junior <wellington.junior@madeiramadeira.com.br>
2022-04-19 23:38:12 -0300

    feature: controller de instalaçao, com logica de rescrita de classe

--- app/Controllers/install/InstallController.php
+++ app/Controllers/install/InstallController.php
@@ -9,35 +9,45 @@ class InstallController extends BaseController
     public function __construct()
     {
         // Call helpers
-        helper(array('form', 'file', 'url'));
+        helper(array('form', 'file', 'url', 'session'));
     }
 
     public function index()
     {
+        $data = array();
+        if (session()->has('error')) {
+            $data['error'] = session('error');
+        }
+        return view('install_form', $data);
+    }
+
+    public function it_started()
+    {
         // Build our checks
         $data = array();
-        // Destroy All Session
-        $session = session();
-        $session->destroy();
+
         // Check for writable folders
         $data['is_writeable']['root'] = is_writeable(FCPATH);
         $data['is_writeable']['config'] = is_writeable(FCPATH . 'app/Config/');
         $data['is_writeable']['uploads'] = is_writeable(FCPATH . 'uploads/');
 
-        $validation =  $this->validate([
+        $rules = [
             'hostname' => 'required',
             'database' => 'required',
             'username' => 'required',
-            'password' => 'required',
-            'prefix'   => 'required'
-        ]);
+            'password' => 'required'
+        ];
 
-        if (!$validation) {
-            // errors
-            return view('install_form');
-            // return redirect()->back()->withInput();
+        if (!$this->validate($rules)) {
+            return redirect()->back()->withInput()->with('error', $this->validator);
         } else {
-            return view('install_form');
+           //setup the database config file
+           $settings                   = array();
+           $settings['hostname']       = $this->request->getVar('hostname');
+           $settings['username']       = $this->request->getVar('username');
+           $settings['password']       = $this->request->getVar('password');
+           $settings['database']       = $this->request->getVar('database');             
+           # $file_contents              = $this->request->getVar('templates/database', $settings, true);
         }
     }
 }
