a13a3cdc6c9edfbfc75eb8a350ff11281b535cf2
Wellington Junior <wellington.junior@madeiramadeira.com.br>
2022-06-02 23:13:02 -0300

    coluna role_id para inserçao

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,14 +7,14 @@ use CodeIgniter\Model;
 class User extends Model
 {
     protected $DBGroup          = 'default';
-    protected $table            = 'users';
+    protected $table            = 'user';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['name','username','password','email','status'];
+    protected $allowedFields    = ['name','username','password','email','role_id','status'];
 
     // Dates
     protected $useTimestamps = false;
