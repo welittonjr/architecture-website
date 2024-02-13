7fac9a40e002bc687a2ec8c7af8e2f8bc41e2f69
Wellington <wellington.junior@madeiramadeira.com.br>
2024-02-13 19:59:41 -0300

    (refactor) refatoração do codigo de depoimentos

--- js/animations.js
+++ js/animations.js
@@ -5,29 +5,29 @@
   Description: Animações para o Site
 */
 
-let sectionService = document.getElementById("service");
-let sectionProject = document.getElementById("project");
-let animationAddedService = false;
-let animationAddedProject = false;
+// let sectionService = document.getElementById("service");
+// let sectionProject = document.getElementById("project");
+// let animationAddedService = false;
+// let animationAddedProject = false;
 
-window.onscroll = () => {
-  let top = window.scrollY;
-  // section services
-  let ssOffset = sectionService.offsetTop - 900;
-  let ssHeight = sectionService.offsetHeight;
+// window.onscroll = () => {
+//   let top = window.scrollY;
+//   // section services
+//   let ssOffset = sectionService.offsetTop - 900;
+//   let ssHeight = sectionService.offsetHeight;
 
-  //section project
-  let spOffset = sectionProject.offsetTop - 900;
-  let spHeight = sectionProject.offsetHeight;
+//   //section project
+//   let spOffset = sectionProject.offsetTop - 900;
+//   let spHeight = sectionProject.offsetHeight;
 
-  if (top >= ssOffset && top < ssOffset + ssHeight && !animationAddedService) {
-    sectionService.classList.add("animation-slide-left");
-    animationAddedService = true;
-  }
+//   if (top >= ssOffset && top < ssOffset + ssHeight && !animationAddedService) {
+//     sectionService.classList.add("animation-slide-left");
+//     animationAddedService = true;
+//   }
 
-  if (top >= spOffset && top < spOffset + spHeight && !animationAddedProject) {
-    sectionProject.classList.add("animation-slide-right");
-    animationAddedProject = true;
-  }
+//   if (top >= spOffset && top < spOffset + spHeight && !animationAddedProject) {
+//     sectionProject.classList.add("animation-slide-right");
+//     animationAddedProject = true;
+//   }
 
-};
+// };
